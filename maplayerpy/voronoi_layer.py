ae90601a2cf092409899874e8b4f591bd5f990a3
lWarne <laurencewarne@gmail.com>
2020-08-26 15:38:50 +0100

    Implement functions for creating voronoi diagrams.

--- maplayerpy/voronoi_layer.py
+++ maplayerpy/voronoi_layer.py
@@ -1,6 +1,9 @@
 """
 """
-from typing import Sequence, Tuple
+import random
+from typing import Callable, Sequence, Tuple
+
+import numpy as np
 
 from .map_layer import BasicLayer
 
@@ -14,6 +17,38 @@ class VoronoiLayer(BasicLayer[int]):
         self.no_points = len(self.points)
 
 
-def get_voronoi_layer(width: int, height: int, no_points: int) -> VoronoiLayer:
+def get_voronoi_layer(
+    width: int,
+    height: int,
+    points: int,
+    distance_fn: Callable[[int, int, int, int], float] = lambda x1, y1, x2, y2: (
+        x1 - x2
+    )
+    * (x1 - x2)
+    + (y1 - y2) * (y1 - y2),
+) -> VoronoiLayer:
     """Creates a Voronoi diagram."""
-    pass
+    arr = np.zeros((height, width))
+    for i in range(height):
+        for j in range(width):
+            arr[i][j] = min(distance_fn(j, i, px, py) for (px, py) in points)
+    return VoronoiLayer(arr, points)
+
+
+def get_random_voronoi_layer(
+    width: int,
+    height: int,
+    no_points: int,
+    distance_fn: Callable[[int, int, int, int], float] = lambda x1, y1, x2, y2: (
+        x1 - x2
+    )
+    * (x1 - x2)
+    + (y1 - y2) * (y1 - y2),
+):
+    """
+    """
+    points = zip(
+        (random.randint(0, width - 1) for i in range(no_points)),
+        (random.randint(0, height - 1) for i in range(no_points)),
+    )
+    return get_voronoi_layer(width, height, points, distance_fn)
