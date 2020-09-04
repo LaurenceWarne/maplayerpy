c5a950332e7326bf73e74089df92576e715a0f7d
lWarne <laurencewarne@gmail.com>
2020-09-04 15:03:57 +0100

    Add more tests for voronoi_layer fns.

--- tests/test_voronoi_layer.py
+++ tests/test_voronoi_layer.py
@@ -6,6 +6,18 @@ from maplayerpy.voronoi_layer import (get_random_voronoi_layer,
                                       get_voronoi_layer)
 
 
+@pytest.mark.voronoi_img
+def test_voronoi_img():
+    import cv2
+    no_points = 20
+    random.seed(1034321)
+    layer = get_random_voronoi_layer(
+        500, 500, no_points,
+    )
+    arr = (256 // no_points) * np.array(layer, dtype=np.int16)
+    cv2.imwrite("examples/voronoi.png", arr)
+
+
 def test_get_voronoi_layer_returns_correct_map():
     layer = get_voronoi_layer(
         4, 4, [(0, 0), (1, 1)],
@@ -14,25 +26,43 @@ def test_get_voronoi_layer_returns_correct_map():
         ) else 1
     )
     assert [
-        [layer[0][0], layer[0][1], layer[0][2], layer[0][3]],
-        [layer[1][0], layer[1][1], layer[1][2], layer[1][3]],
-        [layer[2][0], layer[2][1], layer[2][2], layer[2][3]],
-        [layer[3][0], layer[3][1], layer[3][2], layer[3][3]],
-    ] == [
         [0, 0, 1, 1],
         [0, 1, 1, 1],
         [1, 1, 1, 1],
         [1, 1, 1, 1],
+    ] == [
+        [layer[0][0], layer[0][1], layer[0][2], layer[0][3]],
+        [layer[1][0], layer[1][1], layer[1][2], layer[1][3]],
+        [layer[2][0], layer[2][1], layer[2][2], layer[2][3]],
+        [layer[3][0], layer[3][1], layer[3][2], layer[3][3]],
     ]
 
 
-@pytest.mark.voronoi_img
-def test_voronoi_img():
-    import cv2
-    no_points = 20
-    random.seed(10)
-    layer = get_random_voronoi_layer(
-        500, 500, no_points,
+def test_voronoi_obj_points_are_correct():
+    layer = get_voronoi_layer(
+        4, 4, [(0, 0), (1, 1)]
     )
-    arr = (256 // no_points) * np.array(layer, dtype=np.int16)
-    cv2.imwrite("examples/voronoi.png", arr)
+    assert [(0, 0), (1, 1)] == layer.points
+
+
+def test_voronoi_no_points_is_correct():
+    layer = get_voronoi_layer(
+        4, 4, [(0, 0), (1, 1)]
+    )
+    assert 2 == layer.no_points
+
+
+def test_cannot_set_points_in_voronoi_obj():
+    layer = get_voronoi_layer(
+        4, 4, [(0, 0), (1, 1)]
+    )
+    with pytest.raises(AttributeError):
+        layer.points = [(3, 4)]
+
+
+def test_cannot_set_no_points_in_voronoi_obj():
+    layer = get_voronoi_layer(
+        4, 4, [(0, 0), (1, 1)]
+    )
+    with pytest.raises(AttributeError):
+        layer.no_points = 342
